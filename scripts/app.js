1a14e46d88cb033f22d9c35402537839d305e37e
mednik83 <mmednikov82@gmail.com>
2025-07-08 15:50:44 +0300

    rerender page

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,117 @@
 "use strict";
+
+let habbits = [];
+const HABBIT_KEY = "HABBIT_KEY";
+
+/* page */
+
+const page = {
+  menu: document.querySelector(".menu__list"),
+  header: {
+    h1: document.querySelector(".h1"),
+    progressPercent: document.querySelector(".progress__percent"),
+    progressCoverBar: document.querySelector(".progress__cover-bar"),
+  },
+  habbits: document.querySelector(".habbits"),
+};
+
+/* utils */
+
+function loadData() {
+  const habbitsString = localStorage.getItem(HABBIT_KEY);
+  const habbitArray = JSON.parse(habbitsString);
+  if (Array.isArray(habbitArray)) {
+    habbits = habbitArray;
+  }
+}
+
+function saveData() {
+  localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits));
+}
+// dumbbell-alt.svg
+/* render */
+function rerenderMenu(activeHabbit) {
+  if (!activeHabbit) {
+    return;
+  }
+  for (const habbit of habbits) {
+    const existed = document.querySelector(`[menu-habbit-id="${habbit.id}"]`);
+    if (!existed) {
+      const element = document.createElement("button");
+      element.setAttribute("menu-habbit-id", habbit.id);
+      element.classList.add("menu__item");
+      element.addEventListener("click", () => {
+        rerender(habbit.id);
+      });
+      element.innerHTML = `<img
+                src="./images/icons/${habbit.icon}.svg"
+                alt="${habbit.name}"
+              />`;
+      if (activeHabbit.id === habbit.id) {
+        element.classList.add("menu__item_active");
+      }
+      page.menu.appendChild(element);
+      continue;
+    }
+    if (activeHabbit.id === habbit.id) {
+      existed.classList.add("menu__item_active");
+    } else {
+      existed.classList.remove("menu__item_active");
+    }
+  }
+}
+
+function rerenderHead(activeHabbit) {
+  if (!activeHabbit) {
+    return;
+  }
+
+  page.header.h1.innerText = activeHabbit.name;
+  const progress =
+    activeHabbit.days.length / activeHabbit.target > 1
+      ? 100
+      : (activeHabbit.days.length / activeHabbit.target) * 100;
+  page.header.progressPercent.innerText = progress.toFixed(0) + "%";
+  page.header.progressCoverBar.setAttribute("style", `width: ${progress}%`);
+}
+
+function rerenderMain(activeHabbit) {
+  if (!activeHabbit) {
+    return;
+  }
+
+  page.habbits.innerHTML = "";
+
+  activeHabbit.days.forEach((habbit, index) => {
+    const element = document.createElement("div");
+    element.classList.add("habbit");
+    element.setAttribute("index-habbit", `${index}`);
+    element.innerHTML = `
+      <div class="habbit__day">День ${index + 1}</div>
+      <div class="habbit__comment">${habbit.comment}</div>
+      <button class="habbit__delete">
+        <img src="./images/icons/trash.svg" alt="" />
+      </button>
+      `;
+    page.habbits.appendChild(element);
+  });
+
+  const allHabbits = document.querySelectorAll(".habbit");
+
+  allHabbits[allHabbits.length - 1].querySelector(
+    ".habbit__day"
+  ).innerText = `День ${allHabbits.length}`;
+}
+
+function rerender(activeHabbitId) {
+  const activeHabbit = habbits.find((habbit) => habbit.id === activeHabbitId);
+  rerenderMenu(activeHabbit);
+  rerenderHead(activeHabbit);
+  rerenderMain(activeHabbit);
+}
+
+/* init */
+(() => {
+  loadData();
+  rerender(habbits[0].id);
+})();
